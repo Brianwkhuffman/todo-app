de26bbadcd9c4b4e049dcbe882ab40f204602259
bri <brianwkhuffman@gmail.com>
2020-10-23 14:17:17 -1000

    adds search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import SearchForm from './components/SearchForm.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>
+        <h1>To-Do List</h1>
+      </header>
+      <SearchForm />
     </div>
   );
-}
+};
 
 export default App;
